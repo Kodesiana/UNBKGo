57c0054cc339798052585a7e25464c9e3f87da6a
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2018-07-24 10:49:47 +0700

    demo using RC-1

--- UNBKGo.CLI/Program.cs
+++ UNBKGo.CLI/Program.cs
@@ -1,33 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using UNBKGo.Service;
+using UNBKGo.Service.Net;
 
 namespace UNBKGo.CLI
 {
     class Program
     {
+        private static AutoResetEvent _reset = new AutoResetEvent(false);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting server...");
-            var discoveryService = new UnbkDiscoveryServer();
-            discoveryService.StartDiscovery();
-
-            Console.Read();
-            Console.WriteLine();
-
-            Console.WriteLine("Trying to find server...");
-            var server = new UnbkServer();
-            server.StartListening();
-
-            Console.Read();
-            Console.WriteLine();
+            Task.Run(() => RunTask());
+            _reset.WaitOne();
         }
 
-        
+        private static async void RunTask()
+        {
+            // start server
+            Console.WriteLine("Starting server...");
+            Console.Read();
+            Console.WriteLine();
+
+            var discoveryService = new UnbkServer();
+            discoveryService.Start();
+            
+
+            // find server
+            Console.WriteLine("Trying to find server...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            var client = await UnbkClient.SearchServer();
+            client.MachineShutdown += ClientMachineShutdown;
+            client.ExambroUpdateRequested += Client_ExambroUpdateRequested;
+            
+            // shutdown
+            Console.WriteLine("Shutdown signal...");
+            Console.ReadKey();
+            Console.WriteLine();
+            await discoveryService.Nodes[0].SendShutdownRequest();
+            
+            // shutdown
+            Console.WriteLine("Shutdown signal...");
+            Console.ReadKey();
+            Console.WriteLine();
+            await discoveryService.Nodes[0].SendUpdateRequest();
+
+            Console.ReadKey();
+            _reset.Set();
+        }
+
+        private static void Client_ExambroUpdateRequested(object sender, EventArgs e)
+        {
+            Debug.Print("UPDATE Init");
+        }
+
+        private static void ClientMachineShutdown(object sender, EventArgs e)
+        {
+            Debug.Print("SHUTDOWN Init");
+        }
     }
 }
